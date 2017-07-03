7cba976787177e96963404c5917c03788f909cef
Alexandr Naumov <ultras.1988@mail.ru>
2017-07-03 20:20:46 +0300

    add momento

---
+++ src/behavioral/momento/GithubRepository.java
@@ -0,0 +1,15 @@
+package behavioral.momento;
+
+
+public class GithubRepository {
+
+    private Save save;
+
+    public Save getSave() {
+        return save;
+    }
+
+    public void setSave(Save save) {
+        this.save = save;
+    }
+}
